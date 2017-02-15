67cd314befce804ed20f46e59a2510d06ba6a702
Luca Baldini <luca.baldini@pi.infn.it>
2017-02-15 07:19:30 +0100

    Initial import.

---
+++ Utils/swig/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+import glob
+
+_src = glob.glob('../src/*.cpp')
+_src.append('Utils_wrap.cxx')
+_incl = ['../include']
+
+module = Extension('_Utils', sources=_src, include_dirs=_incl)
+
+setup (name = 'Utils',
+       ext_modules = [module],
+       py_modules = ['Utils']
+)
