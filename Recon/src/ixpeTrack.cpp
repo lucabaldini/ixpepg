c5e0734d8f4600aca9a79d5900b566e36421e671
alberto manfreda <albertom@pi.infn.it>
2017-02-22 19:25:26 +0100

    minor bugs fixed

--- Recon/src/ixpeTrack.cpp
+++ Recon/src/ixpeTrack.cpp
@@ -1,12 +1,33 @@
+/***********************************************************************
+Copyright (C) 2017 the Imaging X-ray Polarimetry Explorer (IXPE) team.
+
+For the license terms see the file LICENSE, distributed along with this
+software.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+***********************************************************************/
+
 #include "ixpeTrack.h"
 
-ixpeTrack::addHit(ixpeHit hit)
+void ixpeTrack::addHit(ixpeHit hit)
 {
   m_hits.push_back(hit);
 }
 
 
-const ixpeTrack::ixpeHit& operator() (int index) const
+const ixpeHit& ixpeTrack::operator() (int index) const
 {
   if (index < 0 || index >= nOfHits())
     throw -1; // FIXME we need a proper exception
