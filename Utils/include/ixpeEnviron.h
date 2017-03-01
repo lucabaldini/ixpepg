dce6fdf9102b5f472fc74ab398d70d5b2d369b9a
alberto manfreda <albertom@pi.infn.it>
2017-03-01 11:45:38 +0100

    first import, environment variables utils

---
+++ Utils/include/ixpeEnviron.h
@@ -0,0 +1,41 @@
+/***********************************************************************
+Copyright (C) 2017 the Imaging X-ray Polarimetry Explorer (IXPE) team.
+
+For the license terms see the file LICENSE, distributed along with this
+software.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+***********************************************************************/
+
+#ifndef IXPEENVIRON_H
+#define IXPEENVIRON_H
+
+#include <cstdlib>
+
+class ixpeEnviron
+{
+
+ public:
+  
+  /// Get the value of an environment variable by name
+  static std::string envVar(const std::string& key)
+  {
+    char* val = std::getenv(key.c_str());
+    return val == NULL ? std::string("") : std::string(val);
+  }
+  
+};
+
+#endif //IXPEENVIRON_H
--- Utils/swig/Utils.i
+++ Utils/swig/Utils.i
@@ -1,7 +1,11 @@
 %module Utils
 
+%include "std_string.i"
+
 %{
 #include "../include/ixpeMath.h"
+#include "../include/ixpeEnviron.h"
 %}
 
 %include "../include/ixpeMath.h"
+%include "../include/ixpeEnviron.h"
